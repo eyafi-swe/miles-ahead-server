5420d5ba09b5be842b5a34d589e53c5a4c6bca9f
Sarwar Azmain Eyafi <75948244+EyafiNeo@users.noreply.github.com>
2022-10-25 22:02:12 +0600

    Blogs loaded

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const cors = require('cors');
+
+app.use(cors());
+const blogs = require('./data/blogs.json');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/blogs',(req,res)=>{
+    res.send(blogs);
 })
 
 app.listen(port, () => {
