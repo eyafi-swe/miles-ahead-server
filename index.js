13f51bb237086e8502e7046280c6bbc25a91e728
Sarwar Azmain Eyafi <75948244+EyafiNeo@users.noreply.github.com>
2022-10-26 19:38:22 +0600

    All Courses data check

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 app.use(cors());
 const blogs = require('./data/blogs.json');
 const courseCategories = require('./data/courses_categories.json');
-
+const courses = require('./data/coursees.json');
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -17,6 +17,9 @@ app.get('/blogs',(req,res)=>{
 
 app.get('/course_categories', (req,res)=>{
     res.send(courseCategories);
+})
+app.get('/courses', (req,res)=>{
+    res.send(courses);
 })
 
 app.listen(port, () => {
